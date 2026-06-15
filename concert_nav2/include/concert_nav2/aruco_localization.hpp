ed27b13112ad1aa7f36de1b12013b357f4805503
lucovizzi <luca.covizzi@robosintesi.com>
2026-06-15 12:37:17 +0200

    forgot to update aruco localization header

--- concert_nav2/include/concert_nav2/aruco_localization.hpp
+++ concert_nav2/include/concert_nav2/aruco_localization.hpp
@@ -1,8 +1,10 @@
 #ifndef CONCERT_NAV2__ARUCO_LOCALIZATION_HPP_
 #define CONCERT_NAV2__ARUCO_LOCALIZATION_HPP_
 
+#include <functional>
 #include <map>
 #include <memory>
+#include <optional>
 #include <string>
 #include <vector>
 
@@ -24,53 +26,70 @@ namespace concert_nav2
 		explicit ArucoLocalization(const rclcpp::NodeOptions &options = rclcpp::NodeOptions());
 
 	private:
-		void loadLandmarks(const std::string &path);
-		void onDetection(const aruco_opencv_msgs::msg::ArucoDetection::SharedPtr msg);
-		void publishPose(
-			const Eigen::Isometry3d &tMapBase, double xyVar, double yawVar,
-			const builtin_interfaces::msg::Time &stamp);
-		rcl_interfaces::msg::SetParametersResult onSetParameters(
-			const std::vector<rclcpp::Parameter> &params);
-
 		struct Landmark
 		{
 			Eigen::Isometry3d pose;
 			double facingYaw;
 		};
-		std::map<int, Landmark> landmarks;
 
-		std::string baseFrame;
-		std::string mapFrame;
+		// Closest acceptable marker observation kept for a single EKF update.
+		struct Candidate
+		{
+			int markerId;
+			double distance;
+			Eigen::Isometry3d mapTbase;
+		};
 
-		
-		double xyVarBase{};
-		double xyVarPerM2{};
-		double yawVarBase{};
-		double yawVarPerM2{};
+		void declareParameters();
+		void registerDoubleParameter(
+			const std::string &name, double defaultValue, double &target, double scale = 1.0);
+		void registerBoolParameter(
+			const std::string &name, bool defaultValue, bool &target);
+		rcl_interfaces::msg::SetParametersResult onSetParameters(
+			const std::vector<rclcpp::Parameter> &params);
+
+		void loadLandmarks(const std::string &path);
+		void onDetection(const aruco_opencv_msgs::msg::ArucoDetection::SharedPtr msg);
+		std::optional<Candidate> evaluateMarker(
+			const aruco_opencv_msgs::msg::MarkerPose &marker, const Eigen::Isometry3d &camTbase);
+		bool hasAcceptableIncidence(
+			const Landmark &landmark, const Eigen::Isometry3d &mapTcam, int markerId);
+		bool passesInnovationGate(const Candidate &candidate);
+		void publishPose(
+			const Candidate &candidate, const builtin_interfaces::msg::Time &stamp);
+
+		std::map<int, Landmark> landmarks_;
+
+		std::string baseFrame_;
+		std::string mapFrame_;
+
+		double xyVarBase_{};
+		double xyVarPerM2_{};
+		double yawVarBase_{};
+		double yawVarPerM2_{};
 
 		// acceptance limits
-		double maxRange{};		  // discard markers farther than this [m]
-		double maxViewAngleRad{}; // discard grazing-incidence views [rad]
-		double maxTiltRad{};	  // max roll+pitch of the best planar candidate [rad]
+		double maxRange_{};		   // discard markers farther than this [m]
+		double maxViewAngleRad_{}; // discard grazing-incidence views [rad]
+		double maxTiltRad_{};	   // max roll+pitch of the best planar candidate [rad]
 
 		// innovation gate vs EKF estimate
-		bool gateEnabled{};
-		double gateXyBase{};
-		double gateYawBase{};
-		double gateGrowthPerSec{};
-		double gateMax{};
-		double relockAfterSec{};
+		bool gateEnabled_{};
+		double gateXyBase_{};
+		double gateYawBase_{};
+		double gateGrowthPerSec_{};
+		double gateMax_{};
+		double relockAfterSec_{};
 
-		bool hasPublished_ = false;
-		bool rejecting_ = false;
-		rclcpp::Time lastAcceptTime_;
-		rclcpp::Time firstRejectTime_;
+		std::optional<rclcpp::Time> lastAcceptTime_;
+		std::optional<rclcpp::Time> rejectingSince_;
 
-		std::shared_ptr<tf2_ros::Buffer> tfBuffer;
-		std::shared_ptr<tf2_ros::TransformListener> tfListener;
-		rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr posePub;
-		std::vector<rclcpp::Subscription<aruco_opencv_msgs::msg::ArucoDetection>::SharedPtr> subs;
-		OnSetParametersCallbackHandle::SharedPtr paramCbHandle;
+		std::shared_ptr<tf2_ros::Buffer> tfBuffer_;
+		std::shared_ptr<tf2_ros::TransformListener> tfListener_;
+		rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr posePub_;
+		std::vector<rclcpp::Subscription<aruco_opencv_msgs::msg::ArucoDetection>::SharedPtr> detectionSubs_;
+		OnSetParametersCallbackHandle::SharedPtr paramCbHandle_;
+		std::map<std::string, std::function<void(const rclcpp::Parameter &)>> parameterUpdaters_;
 	};
 
 } // namespace concert_nav2
